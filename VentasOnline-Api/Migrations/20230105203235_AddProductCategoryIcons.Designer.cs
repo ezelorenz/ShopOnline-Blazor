59a326d117c1b88d6c924c96a93bc8f046a6d0c0
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-01-05 17:48:34 -0300

    Migracion para Iconos

--- VentasOnline-Api/Migrations/20230105203235_AddProductCategoryIcons.Designer.cs
+++ VentasOnline-Api/Migrations/20230105203235_AddProductCategoryIcons.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VentasOnline_Api.Data;
 
@@ -10,9 +11,11 @@ using VentasOnline_Api.Data;
 namespace VentasOnlineApi.Migrations
 {
     [DbContext(typeof(VentasOnlineDbContext))]
-    partial class VentasOnlineDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230105203235_AddProductCategoryIcons")]
+    partial class AddProductCategoryIcons
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VentasOnline-Api/Migrations/20230105203235_AddProductCategoryIcons.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VentasOnlineApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductCategoryIcons : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IconCSS",
+                table: "ProductCategories",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IconCSS",
+                table: "ProductCategories");
+        }
+    }
+}
