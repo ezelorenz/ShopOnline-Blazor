44ab7ed92723a617af2f7f5f26cf45c108874eff
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-01-05 11:23:52 -0300

    Checkout products

---
+++ VentasOnline-Web/Pages/CheckoutBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using VentasOnline.Models.Dto;
+using VentasOnlineWeb.Services;
+
+namespace VentasOnlineWeb.Pages
+{
+    public class CheckoutBase : ComponentBase
+    {
+        protected IEnumerable<CartItemDto> ShoppingCartItems { get; set; }
+        protected decimal PaymentAmount { get; set; }
+
+        [Inject]
+        public IShoppingService ShoppingCartService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+
+            if (ShoppingCartItems != null && ShoppingCartItems.Count() > 0)
+            {
+                PaymentAmount = ShoppingCartItems.Sum(p => p.TotalPrice);
+            }
+        }
+
+
+    }
+}
